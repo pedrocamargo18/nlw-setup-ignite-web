8a8f54479508a2198c892dfb2a3e315585f4757a
Pedro Luz <pedroluz895@gmail.com>
2024-07-24 13:49:13 -0300

    create button and initial function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import logoImage from '../assets/logo.svg'
 import { Plus } from 'phosphor-react'
 
 export function Header(){
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+
+    function buttonClicked(){
+      //abrir modal em tela
+      setIsModalOpen(true)
+
+    }
+
     return (
         <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
         <img src={logoImage} alt="Habits" />
-        <button type='button' className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'>
+
+        {isModalOpen ? <p>Oi</p> : null}
+
+        <button type='button'
+        onClick={buttonClicked}
+        className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'>
           <Plus size={20} className='text-violet-500' />
            Novo Hábito
         </button>
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -4,16 +4,16 @@ import { HabitDay } from "./HabitDay";
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = GenerateDatesFromYearBeginning();
-const minimumSummaryDatesSize = 18 * 7
+const minimumSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDay) => {
+        {weekDays.map((weekDay, i) => {
           return (
-            <div className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center">
+            <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center">
               {weekDay}
             </div>
           );
@@ -25,9 +25,9 @@ export function SummaryTable() {
             return <HabitDay key={date.toString()} />
         })}
 
-        {amountOfDaysToFill > 0 && Array.from({length:amountOfDaysToFill }).map(()=>{
+        {amountOfDaysToFill > 0 && Array.from({length:amountOfDaysToFill }).map((_, i)=>{
             return (
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+                <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
             )
         } ) }
       </div>
