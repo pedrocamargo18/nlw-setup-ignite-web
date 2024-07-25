aa7d9369cbb6a9720aaeea5d3acf13239efae8eb
Pedro Luz <pedroluz895@gmail.com>
2024-07-25 17:28:38 -0300

    create colors and hover to squares

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,5 +1,6 @@
 import * as Popover from "@radix-ui/react-popover";
 import { Progressbar } from "./Progressbar";
+import clsx from "clsx";
 
 
 interface HabitDayProp{
@@ -12,7 +13,15 @@ export function HabitDay( {completed, amount} : HabitDayProp) {
 
   return (
     <Popover.Root>
-      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+      <Popover.Trigger className={clsx('w-10 h-10  border-2  rounded-lg', {
+        'bg-zinc-900 border-zinc-800' : CompletedPercentage === 0,
+        'bg-violet-900 border-violet-700' : CompletedPercentage > 0 && CompletedPercentage < 20,
+        'bg-violet-800 border-violet-600' : CompletedPercentage >= 20 && CompletedPercentage < 40,
+        'bg-violet-700 border-violet-500' : CompletedPercentage >= 40 && CompletedPercentage < 60,
+        'bg-violet-600 border-violet-500' : CompletedPercentage >= 60 && CompletedPercentage < 80,
+        'bg-violet-500 border-violet-400' : CompletedPercentage >= 80,
+      })} />
+
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
           <span className="font-semibold text-zinc-400">Quinta-feira</span>
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -21,7 +21,7 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-2">
         {summaryDates.map(date => {
-            return <HabitDay key={date.toString()} />
+            return <HabitDay completed={4} amount={5}  key={date.toString()} />
         })}
 
         {amountOfDaysToFill > 0 && Array.from({length:amountOfDaysToFill }).map((_, i)=>{
